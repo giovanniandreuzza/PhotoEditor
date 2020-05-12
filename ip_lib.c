7486b0f85529a5842676fbeb2a01df77984f5884
ilenialissa <64855380+ilenialissa@users.noreply.github.com>
2020-05-12 19:19:05 +0200

    Update ip_lib.c

--- ip_lib.c
+++ ip_lib.c
@@ -99,3 +99,154 @@ float get_normal_random(){
     return cos(2*PI*y2)*sqrt(-2.*log(y1));
 
 }
+
+/* Libera la memoria (data, stat e la struttura) */
+void ip_mat_free(ip_mat *a){/*da verificare in base alla create*/
+    unsigned i, j;
+
+    if(a != NULL){
+        free(a->stat);
+
+        for(i=0; i<a->h; i++){
+            for(j=0; j<a->w; j++){
+                free(a->data[i][j]);
+            }
+            free(a->data[i]);
+        }
+        free(a);
+    }    
+}
+
+/* Inizializza una ip_mat con dimensioni w h e k.
+ * Ogni elemento è generato da una gaussiana con media mean e varianza var */
+void ip_mat_init_random(ip_mat * t, float mean, float var){
+    unsigned i, j, k;
+
+    for(i=0; i<t->h; i++)
+        for(j=0; j<t->w; j++)
+            for(k=0; k<t->k; k++){
+                float val = mean+var*get_normal_random();
+                set_val(t, i, j, k, val);
+            }
+}
+
+/* Esegue la somma di due ip_mat (tutte le dimensioni devono essere identiche)
+ * e la restituisce in output. */
+ip_mat * ip_mat_sum(ip_mat * a, ip_mat * b){
+    unsigned i, j, k;
+    ip_mat *out;
+
+    out = ip_mat_create(a->h, a->w, a->k, 0.0);
+
+    if((a->h != b->h) && (a->w != b->w) && (a->k != b->k)){
+        printf("The dimensions are different");
+        exit(1);
+    }
+    else{
+        for(i=0; i<out->h; i++)
+            for(j=0; j<out->w; j++)
+                for(k=0; k<out->k; k++)
+                    set_val(out, i, j, k, (get_val(a, i, k, k) + get_val(b, i, k, k)));
+
+        compute_stats(out);
+        return out;
+    }
+}
+
+/* Calcola la media di due ip_mat a e b e la restituisce in output.*/
+ip_mat * ip_mat_mean(ip_mat * a, ip_mat * b){
+    unsigned i, j, k;
+    ip_mat *out;
+
+    out = ip_mat_create(a->h, a->w, a->k, 0.0);
+
+    if((a->h != b->h) && (a->w != b->w) && (a->k != b->k)){
+        printf("The dimensions are different");
+        exit(1);
+    }
+    else{
+        for(i=0; i<out->h; i++)
+            for(j=0; j<out->w; j++)
+                for(k=0; k<out->k; k++)
+                    set_val(out, i, j, k, (get_val(a, i, k, k) + get_val(b, i, k, k)/2));
+
+        compute_stats(out);
+        return out;
+    }
+
+    /*oppure, ma non credo sia giusto
+    unsigned i, j, k;
+    ip_mat *out;
+
+    out = ip_mat_sum(a, b);
+
+    for(i=0; i<out->h; i++)
+            for(j=0; j<out->w; j++)
+                for(k=0; k<out->k; k++)
+                    set_val(out, i, j, k, get_val(out, i, k, k)/2);*/
+
+    compute_stats(out);
+    return out;
+}
+
+/* Operazione di corruzione con rumore gaussiano:
+ * Aggiunge del rumore gaussiano all'immagine, il rumore viene enfatizzato
+ * per mezzo della variabile amount.
+ * out = a + gauss_noise*amount
+ * */
+ip_mat * ip_mat_corrupt(ip_mat * a, float amount){
+    unsigned i, j, k;
+    ip_mat *out;
+
+    out = ip_mat_create(a->h, a->w, a->k, 0.0);
+
+    for(i=0; i<a->h; i++)
+        for(j=0; j<a->w; j++)
+        {
+            float val = 0.0;
+            for(k=0; k<a->k; k++)
+                val = (get_val(a, i, k, k) + ?gauss_noise? * amount;
+                set_val(out, i, j, k, val);
+        }
+            
+
+    /*aggiungere controllo che i valori siano tra 0 e 255*/
+
+    compute_stats(out);
+    return out;
+}
+
+/* Crea un filtro per rilevare i bordi */
+ip_mat * create_edge_filter(){
+    unsigned i, j, k;
+    ip_mat *out;
+
+    out = ip_mat_create(3, 3, 1, 0.0);
+
+    /*inserire i valori che sono nella tabella a pag. 8 sezione edge, pensa ad un piano cartesiano x,y la z lasciala a 0*/
+    /*prima riga*/
+    set_val(out, 0, 0, 0, -1);
+
+    /*seconda riga*/
+    set_val(out, 0, 1, 0, -1);
+
+    /*terza riga*/
+    set_val(out, 0, 2, 0, -1);
+
+    compute_stats(out);
+    return out;
+}
+
+/* Effettua una riscalatura dei dati tale che i valori siano in [0,new_max].
+ * Utilizzate il metodo compute_stat per ricavarvi il min, max per ogni canale.
+ *
+ * I valori sono scalati tramite la formula valore-min/(max - min)
+ *
+ * Si considera ogni indice della terza dimensione indipendente, quindi l'operazione
+ * di scalatura va ripetuta per ogni "fetta" della matrice 3D.
+ * Successivamente moltiplichiamo per new_max gli elementi della matrice in modo da ottenere un range
+ * di valori in [0,new_max].
+ * */
+void rescale(ip_mat * t, float new_max){
+    
+}
