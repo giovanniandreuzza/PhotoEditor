93476ece1bd4e4667b01c2d64e995e6a98c414fc
Giovanni Andrezza <32865771+giovanniandreuzza@users.noreply.github.com>
2020-05-22 15:47:22 +0200

    Add ip_mat_create

--- ip_lib.c
+++ ip_lib.c
@@ -99,3 +99,36 @@ float get_normal_random(){
     return cos(2*PI*y2)*sqrt(-2.*log(y1));
 
 }
+
+ip_mat * ip_mat_create(unsigned int h, unsigned int w,unsigned  int k, float v) {
+    int i, j, m;
+    ip_mat * ip_mat_new = (ip_mat *) malloc (sizeof(ip_mat *));
+    
+    stats * stat = (stats *) malloc (sizeof(stats *));
+    stat -> min = v;
+    stat -> max = v;
+    stat -> mean = v;
+    
+    float *** data = (float ***) malloc(w * sizeof(float **));
+    for (i = 0; i < w; i++) {
+        data[i] = (float **) malloc (h * sizeof(float *));
+        for (j = 0; j < h; j++) {
+            data[i][j] = (float *) malloc (k * sizeof(float));
+        }
+    }
+
+    for (i = 0; i < w; i++) {
+        for (j = 0; j < h; j++) {
+            for (m = 0; m < k; m++) {
+                data[i][j][m] = v;
+            }
+        }
+    }
+    
+    ip_mat_new -> w = w;
+    ip_mat_new -> h = h;
+    ip_mat_new -> k = k;
+    ip_mat_new -> stat = stat;
+    ip_mat_new -> data = data;
+    return ip_mat_new;
+}
