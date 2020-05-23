bcd131426462adc9af08ebcc9f799ab760f20edc
LCrocco98 <64855270+LCrocco98@users.noreply.github.com>
2020-05-23 18:30:41 +0200

    Update ip_lib.c

--- ip_lib.c
+++ ip_lib.c
@@ -93,7 +93,7 @@ void set_val(ip_mat * a, unsigned int i,unsigned int j,unsigned int k, float v){
     }
 }
 
-float get_normal_random(){
+float get_normal_random(float media, float std){
     float y1 = ( (float)(rand()) + 1. )/( (float)(RAND_MAX) + 1. );
     float y2 = ( (float)(rand()) + 1. )/( (float)(RAND_MAX) + 1. );
     return cos(2*PI*y2)*sqrt(-2.*log(y1));
